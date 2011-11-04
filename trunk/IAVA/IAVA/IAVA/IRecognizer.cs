75976dbe471d99c0820c3f3d24220f491877ee5f
tshrove@gmail.com <tshrove@gmail.com@87f8bf7e-1f98-efaa-0429-fd1e76c188c8>
2011-11-04 01:24:28 +0000

    Changed the archiecture from static classes to classic class architecture. Thanks NICK!!!!

---
+++ trunk/IAVA/IAVA/IAVA/IRecognizer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Iava.Core
+{
+    public interface IRecognizer
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the status of the recognizer.
+        /// </summary>
+        RecognizerStatus Status
+        {
+            get;
+        }
+        #endregion
+
+        #region Static Events
+        /// <summary>
+        /// Raises before the recognizer starts.
+        /// </summary>
+        event EventHandler<EventArgs> Started;
+        /// <summary>
+        /// Raises before the recognizer stops.
+        /// </summary>
+        event EventHandler<EventArgs> Stopped;
+        /// <summary>
+        /// Raises when the recognizer fails.
+        /// </summary>
+        event EventHandler<EventArgs> Failed;
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Starts the recognizer.
+        /// </summary>
+        void Start();
+        /// <summary>
+        ///  Stops the recognizer.
+        /// </summary>
+        void Stop();
+        #endregion
+    }
+}
