b5356388e5a00f472cdd39537c287a07cbcf25a6
tshrove@gmail.com <tshrove@gmail.com@87f8bf7e-1f98-efaa-0429-fd1e76c188c8>
2011-11-04 01:29:50 +0000

    Changed the Audio Recognizer to fit the rest of the architecture.

--- trunk/IAVA/IAVA/Iava.Audio/AudioRecognizer.cs
+++ trunk/IAVA/IAVA/Iava.Audio/AudioRecognizer.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Iava.Core;
 
 namespace Iava.Audio 
 {
-    public class AudioRecognizer 
+    public class AudioRecognizer : Recognizer
     {
+        #region Constructors
+        /// <summary>
+        /// Default Constructor.
+        /// </summary>
+        public AudioRecognizer()
+            :this(null)
+        {
+            // Nothing to do.
+        }
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="filePath"></param>
+        public AudioRecognizer(string filePath)
+            :base(filePath)
+        {
+            // Nothing to do.
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Starts the recognizer.
+        /// </summary>
+        public override void Start()
+        {
+            OnStarted(null, new EventArgs());
+            Status = RecognizerStatus.Running;
+        }
+        /// <summary>
+        ///  Stops the recognizer.
+        /// </summary>
+        public override void Stop()
+        {
+            OnStopped(null, new EventArgs());
+            Status = RecognizerStatus.Ready;
+        }
+        #endregion
     }
 }
