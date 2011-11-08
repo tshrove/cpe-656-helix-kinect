dd71e7717afa9b8854fb50a0872b04bfc5c54f4a
thstinson <thstinson@gmail.com>
2011-11-08 00:56:53 +0000

    Audio Recognizer: Filled out class with simple speech recognition implementation.

--- IAVA/IAVA/Iava.Audio/AudioEventArgs.cs
+++ IAVA/IAVA/Iava.Audio/AudioEventArgs.cs
@@ -7,12 +7,27 @@ namespace Iava.Audio
 {
     public class AudioEventArgs : EventArgs
     {
+        #region Attributes
+
+        /// <summary>
+        /// The audio command that was recognized.
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// The wildcard words (if any present) recognized from the command.
+        /// </summary>
+        public List<string> CommandWilcards { get; set; }
+
+        #endregion
+
         #region Constructor
         /// <summary>
         /// Default Constructor
         /// </summary>
         public AudioEventArgs()
         {
+
         }
         #endregion
     }
--- IAVA/IAVA/Iava.Audio/AudioRecognizer.cs
+++ IAVA/IAVA/Iava.Audio/AudioRecognizer.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Iava.Core;
+using Microsoft.Research.Kinect.Audio;
+using Microsoft.Speech.Recognition;
+using Microsoft.Speech.AudioFormat;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace Iava.Audio 
 {
@@ -11,25 +16,29 @@ namespace Iava.Audio
     /// </summary>
     /// <param name="e"></param>
     public delegate void AudioCallback(AudioEventArgs e);
+    
     /// <summary>
     /// AudioRecognizer Class
     /// </summary>
     public class AudioRecognizer : Recognizer
     {
         #region Constructors
+
         /// <summary>
         /// Constructor.
         /// </summary>
-        /// <param name="filePath"></param>
+        /// <param name="filePath">Path to configuration file</param>
         public AudioRecognizer(string filePath)
             :base(filePath)
         {
             this.AudioCallbacks = new Dictionary<string, AudioCallback>();
             // TODO Add the function to load the gestures to the dictionary.
         }
+
         #endregion
 
         #region Private Properties
+
         /// <summary>
         /// Holds the callbacks for each gesture.
         /// </summary>
@@ -38,38 +47,86 @@ namespace Iava.Audio
             get;
             set;
         }
+
+        #endregion
+
+        #region Private Variables
+
+        /// <summary>
+        /// Reference to the Kinect audio source.
+        /// </summary>
+        private KinectAudioSource audioSource;
+
+        /// <summary>
+        /// Speech recognition engine instance.
+        /// </summary>
+        private SpeechRecognitionEngine speechEngine;
+
+        /// <summary>
+        /// Recognizer ID for the Kinect.
+        /// </summary>
+        private const string RecognizerId = "SR_MS_en-US_Kinect_10.0";
+
+        /// <summary>
+        /// Token source used to stop any background tasks.
+        /// </summary>
+        private CancellationTokenSource tokenSource = new CancellationTokenSource();
+
         #endregion
 
         #region Public Methods
+
         /// <summary>
         /// Starts the recognizer.
         /// </summary>
         public override void Start()
         {
-            Status = RecognizerStatus.Running;
-            OnStarted(this, new EventArgs());  
+            Task t = new Task(SetupAudioDevice, tokenSource.Token);
+            t.Start();
+
+            OnStarted(this, new EventArgs());
         }
+
         /// <summary>
         ///  Stops the recognizer.
         /// </summary>
         public override void Stop()
-        {
+        {           
+            tokenSource.Cancel();
+            tokenSource = new CancellationTokenSource();
+            speechEngine.RecognizeAsyncStop();
+
             Status = RecognizerStatus.Ready;
+
             OnStopped(this, new EventArgs());     
         }
+
         /// <summary>
         /// Used to connect a given delegate to a specified gesture
         /// given by the name.
         /// </summary>
-        /// <param name="name"></param>
-        /// <param name="d"></param>
+        /// <param name="name">The audio command to listen for.</param>
+        /// <param name="callBack">The method to invoke when the spoken command is recognized.</param>
         public void Subscribe(string name, AudioCallback callBack)
         {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Name argument was either null or empty.", "name");
+            }
+
             if (this.AudioCallbacks.ContainsKey(name))
             {
-                this.AudioCallbacks[name] = callBack;
-            }
+                AudioCallbacks.Add(name, callBack);
+                
+                // To update the grammar the recognizer needs to be restarted
+                if (Status == RecognizerStatus.Running)
+                {
+                    Stop();
+                    Start();
+                }
+            }           
         }
+
         /// <summary>
         /// Unsubscribe the given delegate from the given delegate
         /// by the name.
@@ -79,9 +136,149 @@ namespace Iava.Audio
         {
             if (this.AudioCallbacks.ContainsKey(name))
             {
-                this.AudioCallbacks[name] = null;
+                AudioCallbacks.Remove(name);
+
+                // To update the grammar the recognizer needs to be restarted
+                if (Status == RecognizerStatus.Running)
+                {
+                    Stop();
+                    Start();
+                }
+            }            
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Sets up the Kinect Audio Source.
+        /// </summary>
+        private void SetupAudioDevice()
+        {
+            try
+            {
+                RecognizerInfo ri = SpeechRecognitionEngine.InstalledRecognizers().Where(
+                            r => r.Id == RecognizerId).FirstOrDefault();
+
+                if (ri == null)
+                {
+                    throw new Exception("Failed to find any installed audio recognizers.");
+                }
+
+                speechEngine = new SpeechRecognitionEngine(ri.Id);
+
+                speechEngine.LoadGrammar(CreateGrammar());
+                speechEngine.SpeechRecognized += SpeechRecognized;
+                speechEngine.SpeechHypothesized += SpeechHypothesized;
+                speechEngine.SpeechRecognitionRejected += SpeechRejected;
+
+                audioSource = new KinectAudioSource();
+                audioSource.SystemMode = SystemMode.OptibeamArrayOnly;
+                audioSource.FeatureMode = true;
+                audioSource.AutomaticGainControl = false;
+                audioSource.MicArrayMode = MicArrayMode.MicArrayAdaptiveBeam;
+
+                var kinectStream = audioSource.Start();
+                speechEngine.SetInputToAudioStream(kinectStream, new SpeechAudioFormatInfo(
+                                                      EncodingFormat.Pcm, 16000, 16, 1,
+                                                      32000, 2, null));
+                speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+                Status = RecognizerStatus.Running;
+            }
+            catch (OperationCanceledException)
+            {
+                Status = RecognizerStatus.Error;
+            }
+            catch (Exception exception)
+            {
+                // TODO: Log message.  Failed to detect Kinect or start speech engine
+                Status = RecognizerStatus.Error;
+            }           
+        }
+
+
+        /// <summary>
+        /// Creates a Grammar object based on the spoken commands to listen for.
+        /// </summary>
+        /// <returns></returns>
+        private Grammar CreateGrammar()
+        {
+            Grammar rv = null;
+
+            //// Build a simple grammar of commands
+
+            //// TODO: The speech recognition is picking up false positives.  Look into how
+            //// to better refine speech input.
+            Choices choices = new Choices();
+            foreach (string phrase in AudioCallbacks.Keys)
+            {
+                choices.Add(phrase);
+            }
+
+            GrammarBuilder builder = new GrammarBuilder();
+            builder.Append(choices);
+
+            rv = new Grammar(builder);
+            rv.Enabled = true;
+            rv.Name = "IAVA";
+
+            return rv;
+        }
+
+        /// <summary>
+        /// Called when a word or phrase was recognized.
+        /// </summary>
+        /// <param name="sender">Sender of event</param>
+        /// <param name="e">Event args</param>
+        void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.Write("\rSpeech Recognized: \t{0}\tConfidence:\t{1}", e.Result.Text, e.Result.Confidence);
+            foreach (string command in AudioCallbacks.Keys)
+            {
+                if (e.Result.Text.Contains(command))
+                {
+                    try
+                    {
+                        AudioEventArgs args = new AudioEventArgs
+                            {
+                                Command = command,
+                                CommandWilcards = null
+                            };
+
+                        AudioCallbacks[command].Invoke(args);
+                    }
+                    catch
+                    {
+                        //TODO: Log message if this fails
+                    }
+
+                    break;
+                }
             }
         }
+
+        /// <summary>
+        /// Occurs when the speech does not match and commands the recognizer is listening for.
+        /// </summary>
+        /// <param name="sender">Object that send the event</param>
+        /// <param name="e">Event args</param>
+        void SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            Console.WriteLine("\nSpeech Rejected: \t{0}", e.Result.Text);
+        }
+
+        /// <summary>
+        /// Occurs when speech has been hypothesized.
+        /// </summary>
+        /// <param name="sender">Object that send the event</param>
+        /// <param name="e">Event args</param>
+        void SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            Console.Write("\rSpeech Hypothesized: \t{0}\tConfidence:\t{1}", e.Result.Text, e.Result.Confidence);
+        }
+
         #endregion
     }
 }
