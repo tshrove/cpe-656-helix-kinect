93facb5d1ecd49a076ffc3ac0aa0b5a3e3b1811c
tshrove@gmail.com <tshrove@gmail.com@87f8bf7e-1f98-efaa-0429-fd1e76c188c8>
2011-11-09 04:05:05 +0000

    Add the ui project to the solution.

---
+++ trunk/IAVA/IAVA/Iava.Ui/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Iava.Gesture;
+using Iava.Audio;
+
+namespace Iava.Ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Iava.Gesture.GestureRecognizer m_pGestureRecognizer;
+        private Iava.Audio.AudioRecognizer m_pAudioRecognizer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            m_pGestureRecognizer = new Gesture.GestureRecognizer(string.Empty);
+            m_pAudioRecognizer = new Audio.AudioRecognizer(string.Empty);
+
+            m_pGestureRecognizer.Subscribe("Zoom", GestureZoomCallback);
+            m_pAudioRecognizer.Subscribe("Zoom", AudioZoomCallback);
+        }
+        /// <summary>
+        /// The callback for when the zoom gesture is detected.
+        /// </summary>
+        /// <param name="e"></param>
+        private void GestureZoomCallback(GestureEventArgs e)
+        {
+            this.ZoomCallback();
+        }
+        private void AudioZoomCallback(AudioEventArgs e)
+        {
+            this.ZoomCallback();
+        }
+        private void ZoomCallback()
+        {
+            // TODO: This needs to be changed.
+            this.map1.Zoom(2.0);
+        }
+    }
+}
