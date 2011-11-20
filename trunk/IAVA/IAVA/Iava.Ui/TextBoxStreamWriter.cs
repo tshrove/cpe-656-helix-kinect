845fd8e3c15f06a9243021b0c4d7ef00dfbf356e
tshrove@gmail.com <tshrove@gmail.com@87f8bf7e-1f98-efaa-0429-fd1e76c188c8>
2011-11-20 18:52:38 +0000

    Updated the Ui

---
+++ trunk/IAVA/IAVA/Iava.Ui/TextBoxStreamWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Controls;
+
+namespace Iava.Ui
+{
+    public class TextBoxStreamWriter : TextWriter
+    {
+        TextBox _text = null;
+
+        public TextBoxStreamWriter(TextBox outputBox)
+        {
+            _text = outputBox;
+        }
+
+        public override void Write(string value)
+        {
+            base.Write(value);
+            _text.Dispatcher.Invoke(new Action(() => _text.AppendText(value)));
+            _text.Dispatcher.Invoke(new Action(() => _text.ScrollToEnd()));
+        }
+
+        public override Encoding Encoding
+        {
+            get { return System.Text.Encoding.UTF8; }
+        }
+    }
+}
