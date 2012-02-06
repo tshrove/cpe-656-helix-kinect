aa246a47fec07657f65775e6bfcf6965918a31bc
tshrove@gmail.com <tshrove@gmail.com@87f8bf7e-1f98-efaa-0429-fd1e76c188c8>
2012-02-06 02:27:46 +0000

    Added the gesture reader to the project.

--- trunk/GestureRecorder/GestureRecorder/MainWindow.xaml.cs
+++ trunk/GestureRecorder/GestureRecorder/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using tempuri.org.GestureDefinition.xsd;
 
 namespace GestureRecorder {
     /// <summary>
@@ -19,6 +20,18 @@ namespace GestureRecorder {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.ShowDialog();
+            if (System.IO.File.Exists(dlg.FileName))
+            {
+                System.IO.FileStream file = new System.IO.FileStream(dlg.FileName, System.IO.FileMode.Open);
+                System.IO.StreamReader stream = new System.IO.StreamReader(file);
+                Gesture gesture = Gesture.Load(stream);
+            }
         }
     }
 }
