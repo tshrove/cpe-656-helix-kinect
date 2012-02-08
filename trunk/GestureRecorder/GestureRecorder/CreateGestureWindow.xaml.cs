c578beccd8920ba914a710ccfd94d53f8130450b
tshrove@gmail.com <tshrove@gmail.com@87f8bf7e-1f98-efaa-0429-fd1e76c188c8>
2012-02-08 02:30:34 +0000

    Worked on Ui for Gesture Recorder.

---
+++ trunk/GestureRecorder/GestureRecorder/CreateGestureWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GestureRecorder
+{
+    /// <summary>
+    /// Interaction logic for CreateGestureWindow.xaml
+    /// </summary>
+    public partial class CreateGestureWindow : Window
+    {
+        public CreateGestureWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
