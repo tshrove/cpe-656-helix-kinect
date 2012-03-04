9e04c7c758057143404da316edbfe62e892eb020
nick.mallard@gmail.com <nick.mallard@gmail.com@87f8bf7e-1f98-efaa-0429-fd1e76c188c8>
2012-03-04 20:05:05 +0000

    Added Translate function

---
+++ trunk/IAVA/IAVA/Iava.Core/Math/Geometry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Iava.Core.Math {
+    static class Geometry {
+        public static Point Translate(Point original, Point translation) {
+            Point returnPoint = new Point();
+
+            // Translate the Points
+            returnPoint.X = original.X - translation.X;
+            returnPoint.Y = original.Y - translation.Y;
+
+            return returnPoint;
+        }
+    }
+}
+
