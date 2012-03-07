03e8cff863fc7f0da6a65ccf29bbad578b746296
thstinson <thstinson@gmail.com>
2012-03-07 04:18:14 +0000

    Updated the way the AudioRecognizer creates grammars. Added status message logger (no classes currently using it). Minor updates to test driver audio callbacks and timer resets.  Attempting to fix issue where timers count up by making sure timers are only accessed via the UI thread.

---
+++ IAVA/IAVA/Iava.Core/Logging/Message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iava.Core.Logging
+{
+    public enum MessageType
+    {
+        Information,
+        Warning,
+        Error
+    }
+
+    public class Message
+    {
+        public string Text { get; private set; }
+
+        public string Source { get; private set; }
+
+        public MessageType Level { get; private set; }      
+
+        public Exception Exception { get; private set; }
+
+        internal Message(string text, string source, MessageType level, Exception exception = null)
+        {
+            Text = text;
+            Source = source;
+            Level = level;          
+            Exception = exception;
+        }
+    }
+}
--- IAVA/IAVA/Iava.Core/Properties/AssemblyInfo.cs
+++ IAVA/IAVA/Iava.Core/Properties/AssemblyInfo.cs
@@ -34,3 +34,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Iava.Test")]
+[assembly: InternalsVisibleTo("Iava.Audio")]
+[assembly: InternalsVisibleTo("Iava.Gesture")]
+[assembly: InternalsVisibleTo("Iava.Input")]
